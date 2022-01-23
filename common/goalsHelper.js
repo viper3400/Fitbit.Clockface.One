f400effe7ff41bc7a3a9521f967dceb40063ef86
viper3400 <viper_3400@gmx.de>
2022-01-23 10:43:07 +0100

    display "--" if resting heart rate is undefined

--- common/goalsHelper.js
+++ common/goalsHelper.js
@@ -7,45 +7,46 @@ export function getGoalStatistics() {
   let calories = getCaloriesGoal();
   let elevationGain = getElevationGoal();
   let activeZoneMinutes = getActiveZoneMinutesGoal();
-  
+
   const stats = new Statistics(steps, calories, elevationGain, activeZoneMinutes);
   return stats;
 }
 function getStepGoal() {
   if (checkPermission() && goals.steps !== undefined) {
-   return goals.steps;
+    return goals.steps;
   }
   else return 0;
 }
 
 function getCaloriesGoal() {
-    if (checkPermission() && goals.calories  !== undefined) {
-   return goals.calories;
+  if (checkPermission() && goals.calories !== undefined) {
+    return goals.calories;
   }
   else return 0;
 }
 
 function getElevationGoal() {
-    if (checkPermission() && goals.elevationGain  !== undefined) {
-   return goals.elevationGain;
+  if (checkPermission() && goals.elevationGain !== undefined) {
+    return goals.elevationGain;
   }
   else return 0;
 }
 
 function getActiveZoneMinutesGoal() {
-    if (checkPermission() && goals.activeZoneMinutes  !== undefined) {
-   return goals.activeZoneMinutes.total;
+  if (checkPermission() && goals.activeZoneMinutes !== undefined) {
+    return goals.activeZoneMinutes.total;
   }
   else return 0;
 }
 
 export function getRestingHeartRate() {
   if (checkPermission()) {
-    return dayHistory.query()[0].restingHeartRate;
-  } else return 0;
+    if (dayHistory.maxRecordCount > 0) {
+      return dayHistory.query()[0].restingHeartRate;
+    } else return "--"
+
+  } else return "--";
 }
-
-
 
 function checkPermission() {
   return appbit.permissions.granted("access_activity");
